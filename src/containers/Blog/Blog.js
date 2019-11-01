5c40c9741eed652ea696b2c0cada00bc815e077c
Mihaela Corina <phoenixco@users.noreply.github.com>
2019-11-01 13:48:08 +0000

    Add files via upload

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+import post from "../../components/Post/Post";
+
+class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map(post => {
+        return { ...post, author: "Max" };
+      });
+      this.setState({ posts: updatedPosts });
+      console.log(response);
+    });
+  }
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
+  render() {
+    const posts = this.state.posts.map(posts => {
+      return (
+        <Post
+          key={post.id}
+          title={posts.title}
+          author={posts.author}
+          clicked={() => this.postSelectedHandler(posts.id)}
+        ></Post>
+      );
+    });
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Blog;
